3f82e4b6ddca13b905e9e8b1a31a4d7396a61ea0
vcTaz <tsazeides@gmail.com>
2026-09-24 23:34:39 +0300

    feat: solved the greetings assignment utilizing slices and rand

--- Labs/3-Go/cmd/hello/main.go
+++ Labs/3-Go/cmd/hello/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand/v2"
 
 	"github.com/vcTaz/CS-452-Data-Centers/Labs/3-Go/internal/greetings"
 )
+
+const MaxSize = 3
 
 func main() {
 	/*
@@ -15,7 +18,24 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Killijiros")
+	var names []string
+	var name string
+	// depricated: for i := 0; i < MaxSize; i++ {
+	for range MaxSize {
+		fmt.Printf("Enter a name:\n")
+		fmt.Scan(&name)
+		names = append(names, name)
+	}
+
+	/*
+		for i := range MAX_SIZE {
+			fmt.Printf("%v\n", names[i])
+		}
+	*/
+
+	message, err := greetings.Hello(names[rand.IntN(len(names))])
+	//message, err := greetings.Hello(names[rand.Int() % len(names)])
+	//message, err := greetings.Hello("Killijiros")
 	if err != nil {
 		log.Fatal(err)
 	}
